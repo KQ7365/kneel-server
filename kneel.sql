01626f20da1f08d8b10406f46e4347726bc2183a
Kevin Quail <kevinq.7365@gmail.com>
2023-10-26 09:13:17 -0500

    wip

---
+++ kneel.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `Metals` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `metal` NVARCHAR(160) NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Styles` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `style` NVARCHAR(160) NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Sizes` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `caret` NVARCHAR(160) NOT NULL,
+    `price` NUMERIC(5,2) NOT NULL
+);
+
+CREATE TABLE `Orders` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `metal_id` INTEGER NOT NULL,
+    `style_id` INTEGER NOT NULL,
+    `size_id` INTEGER NOT NULL,
+    FOREIGN KEY (`metal_id`) REFERENCES `Metals`(`id`),
+    FOREIGN KEY (`style_id`) REFERENCES `Styles`(`id`),
+    FOREIGN KEY (`size_id`) REFERENCES `Sizes`(`id`)
+);
